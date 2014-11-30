1d5081ea4b4bc45d0de20b11f50742fc9512b396
Markus Malkusch <markus@malkusch.de>
2014-11-30 22:11:24 +0100

    updated api documentation

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","malkusch\\phpmock\\AbstractMock"],["c","malkusch\\phpmock\\DateMock"],["c","malkusch\\phpmock\\MicrotimeMock"],["c","malkusch\\phpmock\\MtRandMock"],["c","malkusch\\phpmock\\RandMock"],["c","malkusch\\phpmock\\TimeMock"]];
+ApiGen.elements = [["c","malkusch\\phpmock\\CallableProvider"],["c","malkusch\\phpmock\\FixedMicrotimeFunction"],["c","malkusch\\phpmock\\FixedValueFunction"],["c","malkusch\\phpmock\\Mock"],["c","malkusch\\phpmock\\MockBuilder"]];
