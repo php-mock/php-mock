37b301b4b479601232f3919920451c6e777c3264
Markus Malkusch <markus@malkusch.de>
2014-12-01 18:54:27 +0100

    mocks are case insensitive and can't be enabled twice at once.

---
+++ classes/MockEnabledException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace malkusch\phpmock;
+
+/**
+ * Exception when enabling a mock for an already mocked function.
+ *
+ * @author Markus Malkusch <markus@malkusch.de>
+ * @link bitcoin:1335STSwu9hST4vcMRppEPgENMHD2r1REK Donations
+ * @license WTFPL
+ * @see Mock::enable()
+ */
+class MockEnabledException extends \Exception
+{
+    
+    /**
+     * Sets the message.
+     * 
+     * @param string $message Exception message.
+     */
+    public function __construct($message)
+    {
+        parent::__construct($message);
+    }
+}
---
+++ tests/MockCaseInsensitivityTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace malkusch\phpmock;
+
+/**
+ * Tests Mock's case insensitivity.
+ *
+ * @author Markus Malkusch <markus@malkusch.de>
+ * @link bitcoin:1335STSwu9hST4vcMRppEPgENMHD2r1REK Donations
+ * @license WTFPL
+ * @see Mock
+ */
+class MockCaseInsensitivityTest extends \PHPUnit_Framework_TestCase
+{
+    
+    /**
+     * @var Mock
+     */
+    private $mock;
+    
+    protected function tearDown()
+    {
+        if (isset($this->mock)) {
+            $this->mock->disable();
+            
+        }
+    }
+
+    /**
+     * @param string $mockName  The mock function name.
+     * 
+     * @expectedException malkusch\phpmock\MockEnabledException
+     * @dataProvider provideTestCaseSensitivity
+     * @test
+     */
+    public function testFailEnable($mockName)
+    {
+        $builder = new MockBuilder();
+        $builder->setNamespace(__NAMESPACE__)
+                ->setName(strtolower($mockName))
+                ->setCallableProvider(new FixedValueFunction(1234));
+
+        $this->mock = $builder->build();
+        $this->mock->enable();
+        
+        $failingMock = $builder->setName($mockName)->build();
+        $failingMock->enable();
+    }
+    
+    /**
+     * Tests case insensitive mocks.
+     * 
+     * @param string $mockName  The mock function name.
+     * 
+     * @test
+     * @dataProvider provideTestCaseSensitivity
+     */
+    public function testCaseSensitivity($mockName)
+    {
+        $builder = new MockBuilder();
+        $builder->setNamespace(__NAMESPACE__)
+                ->setName($mockName)
+                ->setCallableProvider(new FixedValueFunction(1234));
+
+        $this->mock = $builder->build();
+        $this->mock->enable();
+        
+        $this->assertEquals(1234, time(), "time() is not mocked");
+        $this->assertEquals(1234, Time(), "Time() is not mocked");
+        $this->assertEquals(1234, TIME(), "TIME() is not mocked");
+    }
+    
+    /**
+     * Returns test cases for testCaseSensitivity().
+     * 
+     * @return string[][] Test cases.
+     */
+    public function provideTestCaseSensitivity()
+    {
+        return array(
+            array("TIME"),
+            array("Time"),
+            array("time"),
+        );
+    }
+}
