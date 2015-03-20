02139acd32f5bd2df035703034466c0a0ff3fd44
Markus Malkusch <markus@malkusch.de>
2015-03-20 17:50:31 +0100

    test referenced old trait name

--- tests/phpunit/PHPMockTest.php
+++ tests/phpunit/PHPMockTest.php
@@ -3,12 +3,12 @@
 namespace malkusch\phpmock\phpunit;
 
 /**
- * Tests PHPUnitBuilderTrait.
+ * Tests PHPMock.
  *
  * @author Markus Malkusch <markus@malkusch.de>
  * @link bitcoin:1335STSwu9hST4vcMRppEPgENMHD2r1REK Donations
  * @license http://www.wtfpl.net/txt/copying/ WTFPL
- * @see PHPUnitBuilderTrait
+ * @see PHPMock
  */
 class PHPMockTest extends \PHPUnit_Framework_TestCase
 {
