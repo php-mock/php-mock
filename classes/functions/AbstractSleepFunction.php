3b0bc540630b1db9c2e869bdac4fbe936a8621ee
Markus Malkusch <markus@malkusch.de>
2016-07-11 23:54:50 +0200

    Fix typo

--- classes/functions/AbstractSleepFunction.php
+++ classes/functions/AbstractSleepFunction.php
@@ -45,7 +45,7 @@ abstract class AbstractSleepFunction implements FunctionProvider
     }
 
     /**
-     * Converts the sleep() paramater into seconds.
+     * Converts the sleep() parameter into seconds.
      *
      * @param int $amount Amount of time units.
      * @return mixed Seconds.
