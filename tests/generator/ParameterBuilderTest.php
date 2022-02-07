30a8fc587935f5e294a0f2bf3af5431c5bcec95b
Michał Bundyra <contact@webimpress.com>
2022-02-07 15:33:28 +0000

    Add test case with non-existing function

--- tests/generator/ParameterBuilderTest.php
+++ tests/generator/ParameterBuilderTest.php
@@ -124,6 +124,7 @@ class ParameterBuilderTest extends TestCase
         }
 
         $cases = [
+            ["", "", __NAMESPACE__ . "\\testDoesNotExist"],
             ["", "", __NAMESPACE__ . "\\testNoParameter"],
             ['$one', '$one', __NAMESPACE__ . "\\testOneParameter"],
             ['$one, $two', '$one, $two', __NAMESPACE__ . "\\testTwoParameters"],
